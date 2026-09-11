76701c864d6e09857b196a9d4c29bcec8c024e28
lazyxahmedansari-source <lazyxahmedansari@gmail.com>
2026-09-11 06:02:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+  process.exit(0);
+});
 
+process.on('SIGINT', () => {
+  // Local Ctrl+C
+  console.log('[System] Manual stop requested. Exiting...');
+  process.exit(0);
+});
+
+// ============================================================
+// START THE BOT
+// ============================================================
+console.log('='.repeat(50));
+console.log('  Minecraft AFK Bot v2.3 - Bug Fix Edition');
+console.log('='.repeat(50));
+console.log(`Server: ${config.server.ip}:${config.server.port}`);
+console.log(`Version: ${config.server.version}`);
+console.log(`Auto-Reconnect: ${config.utils['auto-reconnect'] ? 'Enabled' : 'Disabled'}`);
+console.log('='.repeat(50));
+
+createBot();
